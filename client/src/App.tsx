1a0a83cae616424fca0f8c818be1f6b8a330fe17
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-02-23 21:35:24 +0100

    add meals list to client page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 import { useMount } from 'react-use';
 
-import type { ICountry } from '@/domain';
+import type { ICountry, IMeal } from '@/domain';
 import api from '@/api';
 
 function App() {
   const [countries, setCountries] = useState<ICountry[]>([]);
+  const [meals, setMeals] = useState<IMeal[]>([]);
 
   useMount(() => {
     api.get<ICountry[]>('/countries').then((response) => {
       setCountries(response.data);
+    });
+    api.get<IMeal[]>('/meals').then((response) => {
+      setMeals(response.data);
     });
   });
 
@@ -22,9 +26,19 @@ function App() {
               <th>Id</th>
               <th>Country</th>
             </tr>
+            <tr>
+              <th>Id</th>
+              <th>Meal</th>
+            </tr>
           </thead>
           <tbody>
             {countries.map(({ id, name }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{name}</td>
+              </tr>
+            ))}
+            {meals.map(({ id, name }) => (
               <tr key={id}>
                 <td>{id}</td>
                 <td>{name}</td>
