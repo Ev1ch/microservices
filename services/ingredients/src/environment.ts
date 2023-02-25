3525ab4cbe811e601bcbc6d5bc44bc45ee3edd36
Yarik <yaroslav.matsapura@gmail.com>
2023-02-25 15:08:19 +0200

    feat: add environment config for ingredients

---
+++ services/ingredients/src/environment.ts
@@ -0,0 +1,11 @@
+import { cleanEnv, num, str } from 'envalid';
+import 'dotenv/config';
+
+const environment = cleanEnv(process.env, {
+  API_VERSION: str(),
+  API_PREFIX: str(),
+  PORT: num(),
+  CLIENT_ROOT: str(),
+});
+
+export default environment;
