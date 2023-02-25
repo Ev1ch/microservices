7dabc9977920e74cb4126fbc210736d72b003401
Yarik <yaroslav.matsapura@gmail.com>
2023-02-25 15:11:43 +0200

    feat: add router

---
+++ services/ingredients/src/router.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import controller from '@/controller';
+import { INGREDIENTS_ROUTES } from '@/routes';
+
+const router = Router();
+
+export default function initRouter() {
+    return router
+      .get(INGREDIENTS_ROUTES.root, controller.getIngredients)
+      .get(INGREDIENTS_ROUTES.one, controller.getIngredient);
+  }
