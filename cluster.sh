f6a82f958670a4657cc61efce56a5b68ae2d16f6
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-02-26 16:53:25 +0100

    fix: images names in build command

--- cluster.sh
+++ cluster.sh
@@ -5,9 +5,9 @@ minikube kubectl -- get po -A
 minikube tunnel
 minikube docker-env
 docker docker build -t cuisines_image services/cuisines/Dockerfile .
-docker docker build -t cuisines_image services/meals/Dockerfile .
-docker docker build -t cuisines_image services/ingredients/Dockerfile .
-docker docker build -t cuisines_image services/client/Dockerfile .
+docker docker build -t meals_image services/meals/Dockerfile .
+docker docker build -t ingredients_image services/ingredients/Dockerfile .
+docker docker build -t client_image services/client/Dockerfile .
 minikube kubectl -- apply -f ./k8s/client
 minikube kubectl -- apply -f ./k8s/meals
 minikube kubectl -- apply -f ./k8s/cuisines
