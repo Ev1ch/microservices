48de458d8f4af0d24d24d03c5e4556ff0ec64124
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-02-26 16:39:32 +0100

    add commands to cluster.sh to deploy

--- cluster.sh
+++ cluster.sh
@@ -1,5 +1,11 @@
 #/bin/sh
 
-minikube start --vm-driver=docker
+minikube start 
 minikube kubectl -- get po -A
 minikube tunnel
+minikube docker-env
+minikube kubectl -- apply -f ./k8s/client
+minikube kubectl -- apply -f ./k8s/meals
+minikube kubectl -- apply -f ./k8s/cuisines
+minikube kubectl -- apply -f ./k8s/ingredients
+minikube kubectl -- proxy 
