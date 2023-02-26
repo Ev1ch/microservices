cd324fb252f73782a5f10b35a73eaf0d73a68a41
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-02-26 16:46:06 +0100

    add commands to build images to cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -4,6 +4,10 @@ minikube start
 minikube kubectl -- get po -A
 minikube tunnel
 minikube docker-env
+docker docker build -t cuisines_image services/cuisines/Dockerfile .
+docker docker build -t cuisines_image services/meals/Dockerfile .
+docker docker build -t cuisines_image services/ingredients/Dockerfile .
+docker docker build -t cuisines_image services/client/Dockerfile .
 minikube kubectl -- apply -f ./k8s/client
 minikube kubectl -- apply -f ./k8s/meals
 minikube kubectl -- apply -f ./k8s/cuisines
