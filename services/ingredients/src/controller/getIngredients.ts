2dff41ecf9ebf17b05ed4093a54a76642335e831
Vlad Zusko <vladzvu11@gmail.com>
2023-03-01 01:29:32 +0200

    fix:(services/ingredients): fix controller method

--- services/ingredients/src/controller/getIngredients.ts
+++ services/ingredients/src/controller/getIngredients.ts
@@ -1,8 +1,8 @@
-import type { GetIngredients } from "@/abstracts/controller";
-import service, { getIngredients } from "@/service";
-import { mapIngredientToJson } from "@/mappers";
+import type { GetIngredients } from '@/abstracts/controller';
+import service from '@/service';
+import { mapIngredientToJson } from '@/mappers';
 
-const getCountries: GetIngredients = async (req, res, next) => {
+const getIngredients: GetIngredients = async (req, res, next) => {
   try {
     const ingredients = await service.getIngredients();
     const json = ingredients.map(mapIngredientToJson);
