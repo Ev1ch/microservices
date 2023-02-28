5d5c76949e56b55bf8cd1c2ec8a1e535e4e1d6e1
Vlad Zusko <vladzvu11@gmail.com>
2023-03-01 01:10:13 +0200

    fix: fix cluster starting script

--- cluster.sh
+++ cluster.sh
@@ -1,19 +1,17 @@
 #/bin/sh
 
-minikube start 
+minikube start
+eval $(minikube -p minikube docker-env)
 minikube kubectl -- get po -A
-minikube tunnel
-minikube docker-env
-docker docker build -t cuisines_image services/cuisines/Dockerfile .
-docker docker build -t meals_image services/meals/Dockerfile .
-docker docker build -t ingredients_image services/ingredients/Dockerfile .
-docker docker build -t client_image services/client/Dockerfile .
+
+docker build -t client_image client/
+docker build -t cuisines_image services/cuisines/
+docker build -t meals_image services/meals/
+docker build -t ingredients_image services/ingredients/
+
 minikube kubectl -- apply -f ./k8s/client
 minikube kubectl -- apply -f ./k8s/meals
 minikube kubectl -- apply -f ./k8s/cuisines
 minikube kubectl -- apply -f ./k8s/ingredients
-# minikube kubectl -- proxy 
-kubectl apply -f k8s/client/ingress.yaml
-kubectl apply -f k8s/meals/ingress.yaml
-kubectl apply -f k8s/cuisines/ingress.yaml
-kubectl apply -f k8s/ingredients/ingress.yaml
+
+minikube tunnel
