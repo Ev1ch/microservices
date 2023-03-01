b9f252bcf0d25736a066d4f0cb134c6679a396d8
Vlad Zusko <vladzvu11@gmail.com>
2023-03-02 00:21:18 +0200

    fix(services): remove useless cors handling

--- services/meals/src/index.ts
+++ services/meals/src/index.ts
@@ -1,13 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 
 import environment from '@/environment';
 import { MEALS_ROOT } from '@/routes';
 import initRouter from '@/router';
 
 const app = express();
-
-app.use(cors({ origin: environment.CLIENT_ROOT }));
 
 app.use(MEALS_ROOT, initRouter());
 
