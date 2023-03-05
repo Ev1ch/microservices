ebd2767f222b4783afbd196541e2d71691b46572
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-03-05 14:17:24 +0100

    add kubectl commands to apply db services

--- cluster.sh
+++ cluster.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 minikube start
-eval $(minikube -p minikube docker-env)
+eval $(minikube -p minikube docker-env | Invoke-Expression)
 minikube addons enable ingress
 minikube kubectl -- get po -A
 
@@ -9,6 +9,10 @@ docker build -t client_image client/
 docker build -t cuisines_image services/cuisines/
 docker build -t meals_image services/meals/
 docker build -t ingredients_image services/ingredients/
+
+minikube kubectl -- apply -f ./k8s/meals-db
+minikube kubectl -- apply -f ./k8s/cuisines-db
+minikube kubectl -- apply -f ./k8s/ingredients-db
 
 minikube kubectl -- apply -f ./k8s/client
 minikube kubectl -- apply -f ./k8s/meals
