013204d6e8772ab34f22264aca973d146d231784
Vlad Zusko <vladzvu11@gmail.com>
2023-03-08 01:48:54 +0200

    fix(services/meals): fix var case

--- services/meals/src/controller/getMealById.ts
+++ services/meals/src/controller/getMealById.ts
@@ -5,8 +5,8 @@ import { mapMealToJson } from '@/mappers';
 const getMealById: GetMealById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const Meal = await service.getMealById(id);
-    const json = mapMealToJson(Meal);
+    const meal = await service.getMealById(id);
+    const json = mapMealToJson(meal);
     res.json(json);
   } catch (error) {
     next(error);
