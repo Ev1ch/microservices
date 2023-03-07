d6a4ac0a226f80c6825a67837d9905136f9e7b3f
Vlad Zusko <vladzvu11@gmail.com>
2023-03-08 01:20:23 +0200

    feat(services): add db initialization and repositories

---
+++ services/ingredients/src/repositories/IngredientsRepository.ts
@@ -0,0 +1,11 @@
+import type { IIngredientsRepository } from '@/abstracts/repositories';
+import { Ingredient } from '@/models';
+import { runner } from '@/database';
+import Repository from './Repository';
+
+export default class IngredientsRepository
+  extends Repository<Ingredient, string>
+  implements IIngredientsRepository
+{
+  protected adapter = runner.manager.getRepository(Ingredient);
+}
