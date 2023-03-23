689374784fa29c2747ad29353870401f9256c399
Yarik <yaroslav.matsapura@gmail.com>
2023-03-23 18:34:12 +0200

    feat: add abstract type for DELETE cuisine service

--- services/cuisines/src/abstracts/service.ts
+++ services/cuisines/src/abstracts/service.ts
@@ -4,4 +4,6 @@ export type GetCuisines = () => Promise<Cuisine[]>;
 
 export type GetCuisineById = (id: string) => Promise<Cuisine | null>;
 
-export type addCuisine = (name: string) => Promise<Cuisine | null>;
+export type AddCuisine = (name: string) => Promise<Cuisine | null>;
+
+export type DeleteCuisine = (id: string) => Promise<Cuisine | null>;
