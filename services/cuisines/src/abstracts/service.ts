01598344d983f4cfff1f043c32d8417681d4a244
Vlad Zusko <vladzvu11@gmail.com>
2023-03-28 20:41:49 +0300

    fix(services/cuisines): fix service typing

--- services/cuisines/src/abstracts/service.ts
+++ services/cuisines/src/abstracts/service.ts
@@ -1,15 +1,15 @@
-import type { Cuisine } from "@/models";
-import type { ICuisineDTO } from "@/domain";
+import type { Cuisine } from '@/models';
+import type { ICuisineDTO } from '@/domain';
 
 export type GetCuisines = () => Promise<Cuisine[]>;
 
-export type GetCuisineById = (id: string) => Promise<Cuisine | null>;
+export type GetCuisineById = (id: string) => Promise<Cuisine>;
 
-export type AddCuisine = (cuisine: ICuisineDTO) => Promise<Cuisine | null>;
+export type AddCuisine = (cuisine: ICuisineDTO) => Promise<Cuisine>;
 
-export type DeleteCuisine = (id: string) => Promise<void | null>;
+export type DeleteCuisineById = (id: string) => Promise<void>;
 
-export type UpdateCuisine = (
+export type UpdateCuisineById = (
   id: string,
-  cuisine: ICuisineDTO
-) => Promise<Cuisine | null>;
+  cuisine: ICuisineDTO,
+) => Promise<Cuisine>;
