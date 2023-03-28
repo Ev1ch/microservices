d44862541646d600d7e66408f6166cbc3444baf8
Vlad Zusko <vladzvu11@gmail.com>
2023-03-28 20:42:14 +0300

    fix(services/cuisines): fix controller typing

--- services/cuisines/src/abstracts/controller.ts
+++ services/cuisines/src/abstracts/controller.ts
@@ -1,22 +1,22 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from 'express';
 
-import type { ICuisine, ICuisineDTO } from "@/domain";
+import type { ICuisine, ICuisineDTO } from '@/domain';
 
 type ControllerMethod<T = {}, K = {}, B = void> = (
   req: Request<T, K, B>,
   res: Response<K>,
-  next: NextFunction
+  next: NextFunction,
 ) => Promise<void>;
 
 export type GetCuisines = ControllerMethod<{}, ICuisine[]>;
 
 export type GetCuisineById = ControllerMethod<{ id: string }, ICuisine>;
 
-export type AddCuisine = ControllerMethod<{}, ICuisineDTO, ICuisine>;
+export type AddCuisine = ControllerMethod<{}, ICuisine, ICuisineDTO>;
 
-export type DeleteCuisine = ControllerMethod<{ id: string }, void>;
+export type DeleteCuisineById = ControllerMethod<{ id: string }, void>;
 
-export type UpdateCuisine = ControllerMethod<
+export type UpdateCuisineById = ControllerMethod<
   { id: string },
   ICuisine,
   ICuisineDTO
