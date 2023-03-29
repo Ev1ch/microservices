f55968bdb5aaf442a79a516a3f963725687ba366
Yarik <yaroslav.matsapura@gmail.com>
2023-03-30 02:01:02 +0300

    feat: add sevice imports for meals and some helping features for meals

--- services/meals/src/service/index.ts
+++ services/meals/src/service/index.ts
@@ -1,9 +1,15 @@
 import getMeals from "./getMeal";
 import getMealById from "./getMealById";
+import addMeal from "./addMeal";
+import deleteMeal from "./deleteMealById";
+import updateMeal from "./updateMealById";
 
-export { getMeals, getMealById };
+export { getMeals, getMealById, addMeal, deleteMeal, updateMeal };
 
 export default {
   getMeals,
   getMealById,
+  addMeal,
+  deleteMeal,
+  updateMeal,
 };
