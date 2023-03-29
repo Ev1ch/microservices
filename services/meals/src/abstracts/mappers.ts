585b7620afbd6637d53076f6197aac5e05df759d
Yarik <yaroslav.matsapura@gmail.com>
2023-03-30 00:58:35 +0300

    feat: add json to entity mapper for meals

--- services/meals/src/abstracts/mappers.ts
+++ services/meals/src/abstracts/mappers.ts
@@ -1,4 +1,6 @@
-import type { Meal } from '@/models';
-import type { IMeal } from '@/domain';
+import type { Meal } from "@/models";
+import type { IMeal } from "@/domain";
 
 export type MapMealToJson = (Meal: Meal) => IMeal;
+
+export type MapJsonToMeal = (cuisine: IMeal) => Meal;
