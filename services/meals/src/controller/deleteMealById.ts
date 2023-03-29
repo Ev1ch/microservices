5a1fc3db8b73bba3fd9fafb50e51b795b6938af2
Yarik <yaroslav.matsapura@gmail.com>
2023-03-30 02:04:19 +0300

    feat: add controllers for meals service

---
+++ services/meals/src/controller/deleteMealById.ts
@@ -0,0 +1,15 @@
+import type { DeleteMealById } from "@/abstracts/controller";
+import service from "@/service";
+import { setError, setResponse } from "@/common/responses";
+
+const deleteMeal: DeleteMealById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await service.deleteMeal(id);
+    setResponse(res, next);
+  } catch (error) {
+    setError(res, next, error);
+  }
+};
+
+export default deleteMeal;
