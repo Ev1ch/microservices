c0e3b87d7803413a73df631c9ca735730b9e9ed0
Yarik <yaroslav.matsapura@gmail.com>
2023-03-30 01:21:40 +0300

    feat: add controllers abstract types for meals service

--- services/meals/src/abstracts/controller.ts
+++ services/meals/src/abstracts/controller.ts
@@ -1,13 +1,19 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from "express";
 
-import type { IMeal } from '@/domain';
+import type { IMeal, IMealDTO } from "@/domain";
 
-type ControllerMethod<T = {}, K = {}> = (
-  req: Request<T>,
+type ControllerMethod<T = {}, K = void, B = void> = (
+  req: Request<T, K, B>,
   res: Response<K>,
-  next: NextFunction,
+  next: NextFunction
 ) => Promise<void>;
 
 export type GetMeals = ControllerMethod<{}, IMeal[]>;
 
 export type GetMealById = ControllerMethod<{ id: string }, IMeal>;
+
+export type AddMeal = ControllerMethod<{}, IMeal, IMealDTO>;
+
+export type DeleteMealById = ControllerMethod<{ id: string }>;
+
+export type UpdateMealById = ControllerMethod<{ id: string }, IMeal, IMealDTO>;
