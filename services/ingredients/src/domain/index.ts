ae4b567f0b87588e42986e85dd6b9c0b821f64d6
Yarik <yaroslav.matsapura@gmail.com>
2023-03-29 13:54:09 +0300

    feat: add json to entity mapper for ingredients

--- services/ingredients/src/domain/index.ts
+++ services/ingredients/src/domain/index.ts
@@ -1,2 +1,3 @@
-export type { default as IEntity } from './IEntity';
-export type { default as IIngredient } from './IIngredient';
+export type { default as IEntity } from "./IEntity";
+export type { default as IIngredient } from "./IIngredient";
+export type { default as IIngredientDTO } from "./IIngredient";
