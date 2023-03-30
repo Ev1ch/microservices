aafe1f716e92a1b28ae55cc7494f982265a90e3e
Yarik <yaroslav.matsapura@gmail.com>
2023-03-30 12:00:12 +0300

    fix: fix index files

--- services/ingredients/src/index.ts
+++ services/ingredients/src/index.ts
@@ -5,11 +5,13 @@ import environment from "@/environment";
 import database from "@/database";
 import { INGREDIENTS_ROOT } from "@/routes";
 import initRouter from "@/router";
+import { dataMiddleware, errorMiddleware } from "@/middlewares";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(INGREDIENTS_ROOT, initRouter());
+app.use(errorMiddleware, dataMiddleware);
 
 database
   .initialize()
