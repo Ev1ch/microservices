5e9d9d8522734c1fe6ea350be410def0abecff83
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-15 15:25:51 +0200

    add helm dep build commands

--- helm.sh
+++ helm.sh
@@ -1,2 +1,8 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm dependency build helm/charts/common
+helm dependency build helm/charts/cuisines
+helm dependency build helm/charts/client
+helm dependency build helm/charts/ingredients
+helm dependency build helm/charts/meals
+helm dep build helm/
 helm upgrade --install local helm
