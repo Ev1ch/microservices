e9b60cbbfa2e1852dc6798eb6376e2a8c70ddf8b
Yarik <yaroslav.matsapura@gmail.com>
2023-04-17 17:29:47 +0300

    fiX: fix cluster.sh file (images namings)

--- cluster.sh
+++ cluster.sh
@@ -12,13 +12,13 @@ minikube kubectl -- delete ingress --all
 minikube kubectl -- delete secret --all
 minikube kubectl -- delete persistentvolumeclaim --all
 
-docker build -t cuisines_migrations_image:0.1 services/cuisines/migrations/
-# docker build -t meals_migrations_image:0.1 services/meals/migrations/
-# docker build -t ingredients_migrations_image:0.1 services/ingredients/migrations/
+docker build -t cuisines-migrations-image:0.1 services/cuisines/migrations/
+docker build -t meals-migrations-image:0.1 services/meals/migrations/
+docker build -t ingredients-migrations-image:0.1 services/ingredients/migrations/
 
-# docker build -t client_image:0.1 client/
-docker build -t cuisines_image:0.1 services/cuisines/
-# docker build -t meals_image:0.1 services/meals/
-# docker build -t ingredients_image:0.1 services/ingredients/
+docker build -t client-image:0.1 client/
+docker build -t cuisines-image:0.1 services/cuisines/
+docker build -t meals-image:0.1 services/meals/
+docker build -t ingredients-image:0.1 services/ingredients/
 
 minikube tunnel
