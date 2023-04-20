310084e782b53360352e7dcd04c447e53d567369
Vlad Zusko <vladzvu11@gmail.com>
2023-04-20 19:33:07 +0300

    fix: fix api response intercept

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -6,4 +6,9 @@ const instance = axios.create({
   baseURL: environment.API_ROOT,
 });
 
+instance.interceptors.response.use((response) => ({
+  ...response,
+  data: response.data.data,
+}));
+
 export default instance;
