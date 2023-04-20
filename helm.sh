35348d82f29ef20cd180231b0c96a12eaeaabd54
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-20 12:17:18 +0200

    remove unused dep builds

--- helm.sh
+++ helm.sh
@@ -1,9 +1,5 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm dependency build helm/charts/common
 helm dependency build helm/charts/cuisines
-helm dependency build helm/charts/client
 helm dependency build helm/charts/ingredients
 helm dependency build helm/charts/meals
-helm dependency build helm/
 helm install local helm
