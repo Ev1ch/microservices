8221ddcbc74cbe1f8ceb30d2a0f70ec2539972c2
Vlad Zusko <vladzvu11@gmail.com>
2023-04-24 23:33:23 +0300

    feat: add state middleware

--- services/cuisines/src/middlewares/index.ts
+++ services/cuisines/src/middlewares/index.ts
@@ -1,2 +1,3 @@
 export { default as errorMiddleware } from './errorMiddleware';
 export { default as dataMiddleware } from './dataMiddleware';
+export { default as stateMiddleware } from './stateMiddleware';
