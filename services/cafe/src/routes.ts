a325aa7f581ecbf8a3df7efa4789ff086a0a7425
Vlad Zusko <vladzvu11@gmail.com>
2023-04-24 23:37:59 +0300

    fix: fix cafe api root

--- services/cafe/src/routes.ts
+++ services/cafe/src/routes.ts
@@ -2,7 +2,7 @@ import environment from '@/environment';
 
 export const API_ROOT = `${environment.API_PREFIX}/${environment.API_VERSION}`;
 
-export const CAFE_ROOT = API_ROOT;
+export const CAFE_ROOT = `${API_ROOT}/cafe`;
 
 export const CAFE_ROUTES = {
   root: '/',
