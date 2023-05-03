f0a072067982fc2bac49bd36af877091b1afbaa7
Vlad Zusko <vladzvu11@gmail.com>
2023-05-04 01:47:33 +0300

    feat: add namespace creating in helm script

--- helm.sh
+++ helm.sh
@@ -1,6 +1,7 @@
 eval $(minikube -p minikube docker-env)
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add istio https://istio-release.storage.googleapis.com/charts
+kubectl create namespace istio-system
 helm install istio-base istio/base -n istio-system
 helm install istiod istio/istiod -n istio-system --wait
 kubectl label namespace default istio-injection=enabled
