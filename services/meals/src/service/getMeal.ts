ec51e046fb1cfd47cc0ed8f38a10127a751e3b8b
Vlad Zusko <vladzvu11@gmail.com>
2023-05-17 01:05:48 +0300

    fix(services/meals): fix grammar form

--- services/meals/src/service/getMeal.ts
+++ services/meals/src/service/getMeal.ts
@@ -1,14 +1,14 @@
 import type { GetMeals } from '@/abstracts/service';
 import { MealsRepository } from '@/repositories';
-import sendMessage from "./kafka"
+import sendMessage from './kafka';
 
 const repository = new MealsRepository();
 
 const getMeals: GetMeals = async () => {
   const meals = await repository.getAll();
 
-  meals.forEach(meal => {
-    sendMessage('GETTED', meal);
+  meals.forEach((meal) => {
+    sendMessage('GOT', meal);
   });
 
   return meals;
--- services/meals/src/service/getMealById.ts
+++ services/meals/src/service/getMealById.ts
@@ -1,16 +1,16 @@
-import type { GetMealById } from "@/abstracts/service";
-import { NotFoundError } from "@/errors";
-import { mealsRepository } from "@/repositories";
-import sendMessage from "./kafka"
+import type { GetMealById } from '@/abstracts/service';
+import { NotFoundError } from '@/errors';
+import { mealsRepository } from '@/repositories';
+import sendMessage from './kafka';
 
 const getMealById: GetMealById = async (id) => {
   const meal = await mealsRepository.getById(id);
 
   if (!meal) {
-    throw new NotFoundError("Meal not found");
+    throw new NotFoundError('Meal not found');
   }
 
-  sendMessage('GETTED', meal);
+  sendMessage('GOT', meal);
 
   return meal;
 };
