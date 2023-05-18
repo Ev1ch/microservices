6ac882c713d16af52422c4019fcb6d2b632dd526
Vlad Zusko <vladzvu11@gmail.com>
2023-05-18 23:32:55 +0300

    feat(services/logger): update topics to subscribe

--- services/logger/src/constants/loggers.ts
+++ services/logger/src/constants/loggers.ts
@@ -2,7 +2,9 @@ import { getEntitiesLogger } from '@/common/logging';
 
 export const TOPIC_TO_LOGGER_MAP = {
   meals: getEntitiesLogger('Meals'),
+  cuisines: getEntitiesLogger('Cuisines'),
+  ingredients: getEntitiesLogger('Ingredients'),
   default: getEntitiesLogger('Default'),
 };
 
-export const TOPICS_TO_SUBSCRIBE = ['meals'];
+export const TOPICS_TO_SUBSCRIBE = ['meals', 'cuisines', 'ingredients'];
