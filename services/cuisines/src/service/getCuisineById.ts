eff0fdf8e9b385df7c184e79d1193bf104beb260
Yarik <yaroslav.matsapura@gmail.com>
2023-05-18 21:57:13 +0300

    feat: add logging for all of the cuisine services

--- services/cuisines/src/service/getCuisineById.ts
+++ services/cuisines/src/service/getCuisineById.ts
@@ -1,6 +1,7 @@
 import type { GetCuisineById } from '@/abstracts/service';
 import { NotFoundError } from '@/errors';
 import { cuisinesRepository } from '@/repositories';
+import sendMessage from './kafka';
 
 const getCuisineById: GetCuisineById = async (id) => {
   const cuisine = await cuisinesRepository.getById(id);
@@ -8,6 +9,8 @@ const getCuisineById: GetCuisineById = async (id) => {
   if (!cuisine) {
     throw new NotFoundError('Cuisine not found');
   }
+
+  sendMessage('GOT', cuisine);
 
   return cuisine;
 };
