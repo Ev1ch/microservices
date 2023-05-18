351cf8a5b11b5a62d5aea2db53c68f21ac4bb038
Yarik <yaroslav.matsapura@gmail.com>
2023-05-18 21:57:59 +0300

    fix: add missed logging for get cuisine service

--- services/cuisines/src/service/getCuisines.ts
+++ services/cuisines/src/service/getCuisines.ts
@@ -1,8 +1,12 @@
 import type { GetCuisines } from '@/abstracts/service';
 import { cuisinesRepository } from '@/repositories';
+import sendMessage from './kafka';
 
 const getCuisines: GetCuisines = async () => {
   const cuisines = await cuisinesRepository.getAll();
+
+  sendMessage('GOT', { id: '-1', name: 'all cuisines' });
+
   return cuisines;
 };
 
